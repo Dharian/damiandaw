2794003428694e956254027b1616e59f8f03cc3e
Dharian <dopi.xd2@gmail.com>
2018-06-15 22:52:32 +0200

    Saludo clase 1

---
+++ programarepo/programarepo/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programarepo
+{
+    class Class1
+    {
+        public void Saludo()
+        { Console.WriteLine("Hola Mundo"); }
+    }
+}
